1a49b69ae733e3e7ec01fa5df73df9f6d2cb5c9d
Dion Potkamp <dion.potkamp@gmail.com>
2020-11-04 16:42:02 +0100

    Update Hogeschooltaal
    add more question types

--- Hogeschooltaal/extensie/select.js
+++ Hogeschooltaal/extensie/select.js
@@ -29,6 +29,50 @@ document.addEventListener('DOMContentLoaded', function() {
      * Als het een meerkeuze vraag is gebruik ik het ID om de juiste knop te vinden.
      * Als er iets ingevuld moet worden dan gebruik ik de tekst
      */
+    // Create new script element
+    function laadSimulateDrag() {
+        if (document.getElementById('JSimulate') === null) {
+            const script = document.createElement('script');
+            script.id = 'JSimulate';
+            script.src = 'https://rawgit.com/jquery/jquery-ui/1-11-stable/external/jquery-simulate/jquery.simulate.js';
+            // Append to the `head` element
+            document.head.appendChild(script);
+        }
+        if (document.getElementById('Jui') === null) {
+            const script = document.createElement('script');
+            script.id = 'Jui';
+            script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js';
+            // Append to the `head` element
+            document.head.appendChild(script);
+        }
+    }
+    // laadSimulateDrag();
+
+    function simulateDrag(sleep_blok, target) {
+       let draggable = sleep_blok.draggable(),
+            droppable = target.droppable(),
+
+            droppableOffset = droppable.offset(),
+            draggableOffset = draggable.offset(),
+            dx = droppableOffset.left - draggableOffset.left,
+            dy = droppableOffset.top - draggableOffset.top;
+
+        draggable.simulate("drag", {
+            dx: dx,
+            dy: dy
+        });
+    }
+
+    function gaNaarDeVolgendeVraag() {
+        setTimeout(function () {
+            $('#next').trigger('click');
+        }, 300);
+    }
+    let old_verder_vraag = verder_vraag;
+    verder_vraag = function () {
+        old_verder_vraag.apply(this, arguments);
+        gaNaarDeVolgendeVraag();
+    };
     function geefHetGoedeAntwoord() {
         for (x in json) {
             if (json[x][0].vraag.id === de_vraag.id) {
@@ -38,19 +82,40 @@ document.addEventListener('DOMContentLoaded', function() {
                         // Meerkeuze
                         if (de_vraag.id_vraag_type === 2 || de_vraag.id_vraag_type === 15) {
                             document.getElementById(json[x][1][a].antwoord.id).style.backgroundColor = "lightgreen";
+                            setTimeout(function () {
+                                $('#'+json[x][1][a].antwoord.id).trigger('click');
+                            }, 300);
                         }
                         // Invulvraag
                         if (de_vraag.id_vraag_type === 1 || de_vraag.id_vraag_type === 16) {
                             document.getElementById('vraagstelling').querySelector('input').value = json[x][1][a].antwoord.tekst;
+                            setTimeout(function () {
+                                $('#verstuur_antwoord').trigger('click');
+                            }, 300);
                         }
                         // Verbeter de zin
                         if (de_vraag.id_vraag_type === 9) {
                             document.getElementById('antwoord_verbeter').value = json[x][1][a].antwoord.tekst;
+                            setTimeout(function () {
+                                $('#verstuur_antwoord').trigger('click');
+                            }, 300);
                         }
-                        // Hiermee trigger je de volgende vraag
-                        // setTimeout(function () {
-                        //     $('#next').trigger('click');
-                        // }, 300);
+                        // Slepen
+                        if (de_vraag.id_vraag_type === 13) {
+                            let target = document.getElementById('vraagstelling').querySelector('span.reciever');
+                            let sleep_blokken = document.getElementById('vraagstelling').querySelectorAll('span.drag_me');
+                            let antwoord = json[x][1][a].antwoord.tekst;
+
+                            for (x in sleep_blokken) {
+                                if (sleep_blokken[x].outerText === antwoord) {
+                                    sleep_blokken[x].style.backgroundColor = "lightgreen";
+                                    // simulateDrag(sleep_blokken[x], target);
+                                }
+                            }
+                            // setTimeout(function () {
+                            //     $('#verstuur_antwoord').trigger('click');
+                            // }, 300);
+                        }
                         return;
                     }
                 }
