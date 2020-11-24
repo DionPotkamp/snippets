466994d8c92b5042bc81f44bbb3d97277cd81444
Dion Potkamp <dion.potkamp@gmail.com>
2020-11-24 16:55:50 +0100

    add missed out panels

--- DarkStack.js
+++ DarkStack.js
@@ -16,41 +16,54 @@
 
     let style = document.createElement('style');
     style.innerHTML=`
-.header, 
-.footer__help, 
+.header,
+.footer__help,
 .footer__usage a {
     display: none;
 }
 
-body, 
-.content, 
-.node-drag__container, 
-.node--selected, 
-.infobar, 
+body,
+.content,
+.node-drag__container,
+.node--selected,
+.modal-content,
+.dialog__footer,
+.infobar,
+.software__box--selected,
 .sidebar__content {
-    background-color: #252525; color: #EEEEEE;
+    background-color: #252525; 
+    color: #EEEEEE;
 }
 
-.breadcrumb__text, 
-.node__column--modified, 
-.node__extension, 
-.navbar__link, 
-.navbar__link svg path, 
+.gallery__button,
+.breadcrumb__text,
+.node__column--modified,
+.node--separator,
+.activity__date,
+.activity__link,
+.node__extension,
+.navbar__link,
+.software__header,
+.navbar__link svg path,
 .navbar__link svg polygon[fill] {
-    color: #EEEEEE; fill: #EEEEEE !important;
+    color: #EEEEEE; 
+    fill: #EEEEEE !important;
 }
 
-.navbar__link:hover, 
-.navbar__link:hover .navbar__label, 
-.navbar__link:hover svg path, 
-.navbar__link:hover svg polygon[fill], 
-.navbar--primary .navbar__item--active .navbar__link, 
+.gallery__button:hover,
+.activity__link:hover,
+.navbar__link:hover,
+.navbar__link:hover .navbar__label,
+.navbar__link:hover svg path,
+.navbar__link:hover svg polygon[fill],
+.navbar--primary .navbar__item--active .navbar__link,
 .navbar--primary .navbar__item:hover .navbar__link {
-    color: #a9a7ff; fill: #a9a7ff !important;
+    color: #a9a7ff; 
+    fill: #a9a7ff !important;
 }
 
-.navbar, 
-.actions, 
+.navbar,
+.actions,
 .footer__usage {
     background-color: #343434;
 }
@@ -70,9 +83,25 @@ body,
 .sidebar__content img {
     filter: brightness(1.75);
 }
-
 .node__column--light, .node__extension {
     opacity: 1;
+}
+
+.node__column--icon img,
+.software__img {
+    filter:grayscale(100%) invert(100%) brightness(1000%);
+}
+
+.progress {
+    background-color: rgba(236, 238, 239, .5);
+}
+
+.thumbnail__sorter--asc {
+    border-bottom: 4px solid #a9a7ff;
+}
+
+.thumbnail__sorter--desc {
+    border-top: 4px solid #a9a7ff;
 }
 
 ::-webkit-scrollbar {
@@ -89,6 +118,7 @@ body,
 
 ::-webkit-scrollbar-thumb:hover {
     background: #343434;
-}`;
+}
+`;
     document.head.appendChild(style);
 })();
